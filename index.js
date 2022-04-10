e8abe5a8af3dc3cdbc955c8092894451453d8772
Kenyansa <kenyansajustine@gmail.com>
2022-04-10 17:57:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "Kenyansa Justine";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase()+"!";
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let shortGreeting = "Welcome, " + "!";
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
